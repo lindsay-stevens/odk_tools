dcbfc7b2d67f1060c7273fa51b48cb3f64d5536f
lindsay stevens <lindsay.stevens.au@gmail.com>
2016-06-08 20:05:19 +1000

    wip: try to fix the pandoc call, cmd not found 2

--- generate_docx.sh
+++ generate_docx.sh
@@ -1,2 +1,2 @@
 # http://stackoverflow.com/a/26304106
-find ./ -iname "*.md" -type f -exec sh -lc 'pandoc "${0}" -o "${0%.md}.docx"' {} \;
+find ./ -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "${0%.md}.docx"' {} \;
